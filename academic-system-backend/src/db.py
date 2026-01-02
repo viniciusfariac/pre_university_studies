9ecef94fe766173632a5d01e21fdd6fca362d472
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-02 18:00:31 -0300

    feat: initial config db

---
+++ academic-system-backend/src/db.py
@@ -0,0 +1,12 @@
+import mariadb
+import dotenv
+
+dotenv()
+def configuracao_banco ():
+    config = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': '',
+        'password': '',
+        'database': ''
+    }

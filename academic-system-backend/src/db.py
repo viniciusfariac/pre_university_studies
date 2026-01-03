9760b46132796802d8373321e663336a133e3c64
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-03 20:04:48 -0300

    feat: conection with postgree bd

--- academic-system-backend/src/db.py
+++ academic-system-backend/src/db.py
@@ -1,12 +1,17 @@
-import mariadb
-import dotenv
+from dotenv import load_dotenv
+import os
+import psycopg2
 
-dotenv()
+load_dotenv()
+
 def configuracao_banco ():
-    config = {
-        'host': 'localhost',
-        'port': 3306,
-        'user': '',
-        'password': '',
-        'database': ''
-    }
+    conexao = psycopg2.connect(
+        database = os.getenv("DB_NAME"),
+        port = os.getenv("DB_PORT"),
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD")
+    )
+    return conexao
+
+configuracao_banco()

f6e5ddfa3ce66725d10ac4a62024af3a64ffef18
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-03 20:33:09 -0300

    feat: init cadastrar_aluno

--- academic-system-backend/src/alunos.py
+++ academic-system-backend/src/alunos.py
@@ -1,2 +1,7 @@
 def cadastrar_aluno():
-    pass
+    try:
+        nome_aluno = input("Digite o nome do aluno: ")
+        endereco = input("Digite o endereço do aluno: ")
+        data_nascimento = input("Digite a data de nascimento do aluno: ")
+    except ValueError:
+        pass

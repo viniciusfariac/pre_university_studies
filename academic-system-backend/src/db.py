99bb3994936a0f617078aa73fdeff4d13897e365
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-03 21:49:16 -0300

    feat: add better connection with bd and end connection with db

--- academic-system-backend/src/db.py
+++ academic-system-backend/src/db.py
@@ -5,13 +5,18 @@ import psycopg2
 load_dotenv()
 
 def configuracao_banco ():
-    conexao = psycopg2.connect(
-        database = os.getenv("DB_NAME"),
-        port = os.getenv("DB_PORT"),
-        host = os.getenv("DB_HOST"),
-        user = os.getenv("DB_USER"),
-        password = os.getenv("DB_PASSWORD")
-    )
-    return conexao
+    try:
+        conexao = psycopg2.connect(
+            database = os.getenv("DB_NAME"),
+            port = os.getenv("DB_PORT"),
+            host = os.getenv("DB_HOST"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD")
+        )
+        return conexao
+    except ValueError as e:
+        print(f"Erro de conexão com banco de dados {e}")
 
-configuracao_banco()
+def encerra_conexao(conexao):
+    if conexao:
+        conexao.close()

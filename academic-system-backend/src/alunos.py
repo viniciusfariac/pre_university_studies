d4ff1fafb8a695a1bce33e04b4b57309f85c16b4
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-03 21:50:15 -0300

    feat: Receive data and verify

--- academic-system-backend/src/alunos.py
+++ academic-system-backend/src/alunos.py
@@ -1,7 +1,75 @@
+from db import configuracao_banco, encerra_conexao
+from datetime import datetime
+
 def cadastrar_aluno():
     try:
-        nome_aluno = input("Digite o nome do aluno: ")
-        endereco = input("Digite o endereço do aluno: ")
-        data_nascimento = input("Digite a data de nascimento do aluno: ")
+        nome_aluno, endereco, data_nascimento, turma = receber_dados()
+        now = datetime.now()
+        insert_matricula = "INSERT INTO Matricula (data_matricula, turmaID)"
     except ValueError:
         pass
+
+def receber_dados():
+    while True:
+        try:
+            nome_aluno = input("Digite o nome do aluno: ")
+            endereco = input("Digite o endereço do aluno: ")
+
+            data_nascimento = input("Digite a data de nascimento do aluno: ")
+            data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y").date()
+
+            curso_nome = input("Digite o curso que o aluno faz parte: ")
+            curso_id = verifica_curso(curso_nome)
+
+            turma_nome = input("Digite a turma que o aluno faz parte: ")
+            turma_id = verifica_turma(turma_nome)
+
+            if curso_id and turma_id:
+                return nome_aluno, endereco, data_nascimento, turma_id, curso_id
+        except ValueError as e:
+            print(e)
+
+def verifica_turma(turma):
+    try:
+        conexao = configuracao_banco()
+        cursor = conexao.cursor()
+        
+        query ="""SELECT turmaID 
+            FROM Turma 
+            WHERE name = %s"""
+        
+        cursor.execute(query, (turma, ))
+        
+        resultado = cursor.fetchone()
+        if resultado != None:
+            encerra_conexao(cursor)
+            encerra_conexao(conexao)
+            return resultado[0]
+        else:
+            print("Digite uma turma válida")
+
+    except ValueError as e:
+        print(e)
+
+def verifica_curso(curso):
+    try:
+        conexao = configuracao_banco()
+        cursor = conexao.cursor()
+
+        query = """SELECT cursoID
+            FROM Cursos
+            WHERE name = %s"""
+        
+        cursor.execute(query, (curso, ))
+        resultado = cursor.fetchone()
+        
+        if resultado != None:
+            encerra_conexao(cursor)
+            encerra_conexao(conexao)
+            return resultado[0]
+        else:
+            print("Digite um curso válido")
+    except ValueError as e:
+        print(e)
+
+cadastrar_aluno()

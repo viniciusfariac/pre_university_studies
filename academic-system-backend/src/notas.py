d0cbf7d15b6eba669e0d8f256871b52ea47e2ca3
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-06 21:05:58 -0300

    feat: add search student and material

--- academic-system-backend/src/notas.py
+++ academic-system-backend/src/notas.py
@@ -1,5 +1,49 @@
+from db import configuracao_banco, encerra_conexao
 def lancar_nota ():
+    conexao = configuracao_banco()
+    cursor = conexao.cursor()
     quantidade_nota = input("Digite quantas notas gostaria de colocar: ")
+    nome_aluno = input("Digite o aluno que vai receber a(s) nota(s): ")
+    for i in range(quantidade_nota):
+        nota = input(f"Digite a {i - 1} nota: ")
+        materia = input(f"Digite a matéria da {i - 1}")
 
-    for i in range(quantidade_nota()):
-        pass
+        materia_id = pesquisar_materia(materia, cursor)
+        aluno_id = pesquisar_aluno(nome_aluno, cursor)
+        
+
+def pesquisar_aluno(nome_aluno, cursor):
+    try:
+        query ="""SELECT alunoid
+            FROM alunos
+            WHERE name = %s"""
+        
+        cursor.execute(query, (nome_aluno, ))
+        
+        resultado = cursor.fetchone()
+        if resultado != None:
+            encerra_conexao(cursor)
+            return resultado[0]
+        else:
+            print("Digite o nome de um aluno válido")
+
+    except ValueError as e:
+        print(e)
+
+def pesquisar_materia(materia, cursor):
+    try:
+        query ="""SELECT materiaID
+            FROM Materia
+            WHERE name = %s"""
+        
+        cursor.execute(query, (materia, ))
+        
+        resultado = cursor.fetchone()
+        if resultado != None:
+            encerra_conexao(cursor)
+            return resultado[0]
+        else:
+            print("Digite o nome de uma matéria válida")
+
+    except ValueError as e:
+        print(e)

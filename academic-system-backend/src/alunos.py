971d4a776d85847c119251199794dfb900df1397
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-08 18:00:53 -0300

    fix: improved treatment and functions

--- academic-system-backend/src/alunos.py
+++ academic-system-backend/src/alunos.py
@@ -1,40 +1,35 @@
 from db import configuracao_banco, encerra_conexao
 from datetime import datetime
+import psycopg2
 
 def cadastrar_aluno():
     try:
         conexao = configuracao_banco()
         cursor = conexao.cursor()
-        nome_aluno, endereco, data_nascimento, turma_id, curso_id = receber_dados()
         now = datetime.now()
-        id_matricula = cadastrar_matricula(turma_id, now)
+        nome_aluno, endereco, data_nascimento, turma_id= receber_dados(cursor, conexao)
+
+        id_matricula = cadastrar_matricula(turma_id, now, cursor, conexao)
+
         query = "INSERT INTO alunos (name, endereco, data_nascimento, matriculaID) VALUES (%s, %s, %s, %s)"
         values_alunos = nome_aluno, endereco, data_nascimento, id_matricula
         cursor.execute(query, values_alunos)
         conexao.commit()
-        encerra_conexao(conexao)
-        encerra_conexao(cursor)
 
-    except ValueError:
-        pass
-def cadastrar_matricula(turma, now):
-    try:
-        conexao = configuracao_banco()
-        cursor = conexao.cursor()
-        query = """INSERT INTO Matricula (data_matricula, turmaID) 
-        VALUES (%s, %s)
-        RETURNING matriculaID"""
-        cursor.execute(query, (now, turma))
-        id = cursor.fetchone()[0]
-        conexao.commit()
-        print("Dados inseridos com Sucesso")
-        encerra_conexao(conexao)
-        encerra_conexao(cursor)
-        return id
     except ValueError as e:
-        print(f"Erro para cadastrar matricula {e}")
+        print("Erro de entrada", e)
 
-def receber_dados():
+    except psycopg2.Error as e:
+        print("Erro no banco", e)
+
+    except Exception as e:
+        print("Erro inesperado", e)
+
+    finally:
+        encerra_conexao(conexao)
+        encerra_conexao(cursor)
+
+def receber_dados(cursor, conexao):
     while True:
         try:
             nome_aluno = input("Digite o nome do aluno: ")
@@ -44,58 +39,65 @@ def receber_dados():
             data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y").date()
 
             curso_nome = input("Digite o curso que o aluno faz parte: ")
-            curso_id = verifica_curso(curso_nome)
+            curso_id = pesquisar_id("Cursos", "curso", curso_nome, cursor)
 
             turma_nome = input("Digite a turma que o aluno faz parte: ")
-            turma_id = verifica_turma(turma_nome)
+            turma_id = pesquisar_id("Turma", "turma", turma_nome, cursor)
+
 
             if curso_id and turma_id:
-                return nome_aluno, endereco, data_nascimento, turma_id, curso_id
+                return nome_aluno, endereco, data_nascimento, turma_id
         except ValueError as e:
-            print(e)
+            print("Erro de entrada", e)
 
-def verifica_turma(turma):
+        except psycopg2.Error as e:
+            print("Erro no banco", e)
+
+        except Exception as e:
+            print("Erro inesperado", e)
+
+
+def cadastrar_matricula(turma, now, cursor, conexao):
     try:
-        conexao = configuracao_banco()
-        cursor = conexao.cursor()
-        
-        query ="""SELECT turmaID 
-            FROM Turma 
+        query = """INSERT INTO Matricula (data_matricula, turmaID) 
+        VALUES (%s, %s)
+        RETURNING matriculaID"""
+        cursor.execute(query, (now, turma))
+
+        id = cursor.fetchone()[0]
+        conexao.commit()
+
+        print("Dados inseridos com Sucesso")
+        return id
+    except ValueError as e:
+        print("Erro de entrada", e)
+    except psycopg2.Error as e:
+        print("Erro no banco", e)
+    except Exception as e:
+        print("Erro inesperado", e)
+
+def pesquisar_id(tabela, entidade_id, entidade, cursor):
+    try:
+        query =f"""SELECT {entidade_id}ID
+            FROM {tabela}
             WHERE name = %s"""
         
-        cursor.execute(query, (turma, ))
+        cursor.execute(query, (entidade, ))
         
         resultado = cursor.fetchone()
+
         if resultado != None:
-            encerra_conexao(cursor)
-            encerra_conexao(conexao)
             return resultado[0]
         else:
-            print("Digite uma turma válida")
+            print(f"Nome inválido na tabela {tabela}")
+
 
     except ValueError as e:
-        print(e)
-
-def verifica_curso(curso):
-    try:
-        conexao = configuracao_banco()
-        cursor = conexao.cursor()
-
-        query = """SELECT cursoID
-            FROM Cursos
-            WHERE name = %s"""
-        
-        cursor.execute(query, (curso, ))
-        resultado = cursor.fetchone()
-        
-        if resultado != None:
-            encerra_conexao(cursor)
-            encerra_conexao(conexao)
-            return resultado[0]
-        else:
-            print("Digite um curso válido")
-    except ValueError as e:
-        print(e)
+        print("Erro de entrada", e)
+    except psycopg2.Error as e:
+        print("Erro no banco", e)
+    except Exception as e:
+        print("Erro inesperado", e)
 
 if __name__ == "__main__":
     cadastrar_aluno()

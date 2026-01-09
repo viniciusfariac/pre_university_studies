292b42afc8af9536012b56792c1108ce1068e182
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-09 17:03:58 -0300

    feat: add graph of notes and improve function media

--- academic-system-backend/src/notas_calculos.py
+++ academic-system-backend/src/notas_calculos.py
@@ -1,50 +1,77 @@
 from db import encerra_conexao, configuracao_banco
-from notas import pesquisar_id
+from notas import solicitar_aluno, solicitar_materia
+import psycopg2
+from  matplotlib import pyplot as plt
 def media():
     conexao = configuracao_banco()
     cursor = conexao.cursor()
 
     try:
-        aluno_id, materias_id = solicitar_dados(cursor)
-        media = calcular_media(cursor, aluno_id, materias_id)
+        aluno_id = solicitar_aluno(cursor)
+        materia_id = solicitar_materia(cursor)
+        media = calcular_media(cursor, aluno_id, materia_id)
 
         if media is None:
             print("Nenhuma nota encontrada para essas meterias")
         else:
-            print(f"Media: {media}")
+            print(f"Media: {media:.2f}")
     finally:
         encerra_conexao(conexao)
         encerra_conexao(cursor)
-
-def relatorio():
-    pass
-
-def solicitar_dados(cursor):
-    while True:
-        try:
-            nome_aluno = input("Nome aluno: ")
-            id_aluno = pesquisar_id("alunos", "aluno", nome_aluno, cursor)
-            materias = input("Digite as materias, separadas por espaço: ").split()
-            materias_id = []
-            for materia in materias:
-                try:
-                    materia_id = pesquisar_id("Materia", "materia", materia, cursor)
-                    materias_id.append(materia_id)
-                except ValueError as e:
-                    print(f"Materia invalida {materia}", e)
-                    materia = input("Digite a materia novamente: ")
-            return id_aluno, materias_id
-        except ValueError as e:
-            print("Erro de entrada", e)
 
 def calcular_media(cursor, aluno_id, materias_id):
     query = """
         SELECT AVG(nota)
         FROM nota
         WHERE alunoId = %s
-          AND materiaId = ANY(%s)
+        AND materiaId = %s
     """
 
     cursor.execute(query, (aluno_id, materias_id))
     resultado = cursor.fetchone()
     return resultado[0]
+    
+def relatorio():
+    conexao = configuracao_banco()
+    cursor = conexao.cursor()
+
+    try:
+        aluno_id = solicitar_aluno(cursor)
+        materia_id = solicitar_materia(cursor)
+        notas = buscar_nota_relatorio(cursor, aluno_id, materia_id)
+        gerar_grafico(notas)
+    finally:
+        encerra_conexao(conexao)
+        encerra_conexao(cursor)
+
+def buscar_nota_relatorio(cursor, aluno_id, materia_id):
+    try:
+        query = f"""SELECT nota FROM Nota
+        WHERE alunoID = %s
+        AND materiaID = %s"""
+
+        cursor.execute(query, (aluno_id, materia_id))
+
+        resultado = cursor.fetchall()
+        if resultado is not None:
+            return converter_notas(resultado)
+        else:
+            print("Aluno não tem nota nessa matéria")
+    
+    except psycopg2.Error as e:
+        print("Erro no banco", e)
+    finally:
+        encerra_conexao(cursor)
+    
+
+def converter_notas(raw_notas):
+    return [nota[0] for nota in raw_notas]
+
+def gerar_grafico(notas):
+    plt.plot(notas, marker='o')
+    plt.title("Notas dos alunos")
+    plt.xlabel("Avaliações")
+    plt.ylabel("Nota")
+    plt.ylim(0, 10)
+    plt.grid(True)
+    plt.show()

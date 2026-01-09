7a66fdf3c922083bc5073cdb00ee20cb34a1cb2a
viniciusfariac <vfariacerqueira@gmail.com>
2026-01-09 17:55:44 -0300

    feat: The functions for requesting data and searching for IDs have been rewritten.

--- academic-system-backend/src/alunos.py
+++ academic-system-backend/src/alunos.py
@@ -1,13 +1,20 @@
 from db import configuracao_banco, encerra_conexao
 from datetime import datetime
 import psycopg2
+from notas import pesquisar_disponiveis
 
 def cadastrar_aluno():
     try:
         conexao = configuracao_banco()
         cursor = conexao.cursor()
         now = datetime.now()
-        nome_aluno, endereco, data_nascimento, turma_id= receber_dados(cursor, conexao)
+        nome_aluno = input("Nome aluno: ")
+        endereco = input("Endereço: ")
+        data_nascimento = solicitar_data()
+        curso = solicitar_curso(cursor)
+        turma_id = solicitar_turma(cursor)
+        
+
 
         id_matricula = cadastrar_matricula(turma_id, now, cursor, conexao)
 
@@ -28,34 +35,6 @@ def cadastrar_aluno():
     finally:
         encerra_conexao(conexao)
         encerra_conexao(cursor)
-
-def receber_dados(cursor, conexao):
-    while True:
-        try:
-            nome_aluno = input("Digite o nome do aluno: ")
-            endereco = input("Digite o endereço do aluno: ")
-
-            data_nascimento = input("Digite a data de nascimento do aluno: ")
-            data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y").date()
-
-            curso_nome = input("Digite o curso que o aluno faz parte: ")
-            curso_id = pesquisar_id("Cursos", "curso", curso_nome, cursor)
-
-            turma_nome = input("Digite a turma que o aluno faz parte: ")
-            turma_id = pesquisar_id("Turma", "turma", turma_nome, cursor)
-
-
-            if curso_id and turma_id:
-                return nome_aluno, endereco, data_nascimento, turma_id
-        except ValueError as e:
-            print("Erro de entrada", e)
-
-        except psycopg2.Error as e:
-            print("Erro no banco", e)
-
-        except Exception as e:
-            print("Erro inesperado", e)
-
 
 def cadastrar_matricula(turma, now, cursor, conexao):
     try:
@@ -86,11 +65,10 @@ def pesquisar_id(tabela, entidade_id, entidade, cursor):
         
         resultado = cursor.fetchone()
 
-        if resultado != None:
-            return resultado[0]
-        else:
-            print(f"Nome inválido na tabela {tabela}")
-
+        if not resultado:
+            raise ValueError
+        
+        return resultado[0]
 
     except ValueError as e:
         print("Erro de entrada", e)
@@ -98,6 +76,41 @@ def pesquisar_id(tabela, entidade_id, entidade, cursor):
         print("Erro no banco", e)
     except Exception as e:
         print("Erro inesperado", e)
+    
 
+def solicitar_data():
+    while True:
+        try:
+            data_nascimento = input("Digite a data de nascimento do aluno: ")
+            data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y").date()
+            return data_nascimento
+        except ValueError as e:
+            print("Data inválida, data deve ser: d-m-y")
+
+def solicitar_curso(cursor):
+    while True:
+        try:
+            curso_nome = input("Digite o curso que o aluno faz parte: ")
+            resultado = pesquisar_id("Cursos", "curso", curso_nome, cursor)
+            if resultado is not None:
+                return resultado
+            else:
+                print("Cursos disponiveis abaixo: ")
+                print(pesquisar_disponiveis("Cursos", "name", cursor))
+        except ValueError as e:
+            print(" ")
+
+def solicitar_turma (cursor):
+    while True:
+        try:
+            nome_turma = input(f"Digite a turma: ")
+            resultado = pesquisar_id("Turma", "turma", nome_turma, cursor)
+            if resultado is not None:
+                return resultado
+            else:
+                print("Turmas disponiveis abaixo: ")
+                print(pesquisar_disponiveis("Turma", "name", cursor))
+        except ValueError as e:
+            print(" ")
 if __name__ == "__main__":
     cadastrar_aluno()
